145b8c3b16e47810ed016cf40093b3e3e567d020
JOANNA HALL <joanna@JOANNAs-MacBook-Pro.local>
2022-02-17 20:48:28 -0500

    pushed up to Github for preliminary review

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 // TODO: Declare any global variables we need
-
+let headsRolled = 0;
+let tailsRolled = 0;
 
 document.addEventListener('DOMContentLoaded', function () {
     // This is just a sanity check to make sure your JavaScript script is getting loaded
@@ -7,6 +8,39 @@ document.addEventListener('DOMContentLoaded', function () {
     console.log('Hi')
 
     // TODO: Add event listener and handler for flip and clear buttons
+
+    //get required Elements
+    const flipBtn = document.querySelector('#flip');
+    const pennyImg = document.getElementById('penny-image');
+    const message = document.getElementById('message');
+    const tails = document.getElementById('tails');
+    const heads = document.getElementById('heads');
+    const tailsPerc = document.getElementById('tails-percent');
+    const headsPerc = document.getElementById('heads-percent');
+
+
+    flipBtn.addEventListener('click',()=>{
+        //todo:Determine flip outcome
+            let flippedHeads = Math.random() < 0.5
+
+            if(flippedHeads){
+                // if it's heads
+                pennyImg.src = 'assets/images/penny-heads.jpg';
+                message.textContent = "You Flipped Heads!"
+                headsRolled++
+            }else{
+                //if it's tails
+                pennyImg.src = 'assets/images/penny-tails.jpg';
+                message.textContent = "You Flipped Tails!"
+                tailsRolled++
+              } 
+
+                heads.textContent = headsRolled;
+                tails.textContent = tailsRolled;
+
+    })
+
+    
 
     // Flip Button Click Handler
         // TODO: Determine flip outcome
